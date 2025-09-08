24b4e619876da6fd1fedfa66a6dac5bf72125a20
Chinmay-24 <naiks.chinmay@gmail.com>
2025-09-08 19:25:21 +0530

    git commit

--- src/app/projects/BoardView/index.tsx
+++ src/app/projects/BoardView/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { Task as TaskType } from "@/state/api";
+import { EllipsisVertical, Plus } from "lucide-react";
 
 type BoardProps = {
     id: string;
@@ -74,9 +75,39 @@ const TaskColumn = ({
             ref={(instance) => {
                 drop(instance);
             }}
-            className="{`sl:py-4 rounded-lg py-2 xl:px-2 ${isOver ?" } `}"
-        ></div>
-    )
-}
+            className={`sl:py-4 rounded-lg py-2 xl:px-2 ${isOver ? " bg-blue-100 dark:bg-neutral-950": ""}`}
+        >
+            <div className="mb-3 flex w-full">
+                <div
+                className={`w-2 !bg-[${statusColor[status]}] rounded s-lg`}
+                style={{backgroundColor: statusColor[status] }}
+                />
+                <div className="flex w-full items-center justify-between rounded-lg bg-white px-5 py-4 dark:bg-dark-secondary">
+                    <h3 className="flex items-center text-lg font-semibold dark:text-white">
+                        {status}{" "}
+                    
+                    <span
+                        className="ml-2 inline-block rounded-full bg-gray-200 p-1 text-center text-sm leading-none dark:bg-dark-tertiary"
+                        style={{ width: "1.5rem", height: "1.5 rem" }}
+                    >
+                        {tasksCount}
+                    </span>
+                    </h3>
+                    <div className="flex-items-center gap-1">
+                        <button className="flex h-6 w-5 items-center justify-center dark:text-neutral-500">
+                            <EllipsisVertical size= {26} />
+                        </button>
+                        <button className="flex h-6 w-6 items-center justify-center rounded b-gray-200 dark:bg-dark-tertiary dark:text-white"
+                        onClick={() => setIsModalNewTaskOpen(true)}>
+                            <Plus size={16} />
+
+                        </button>
+                        </div>
+                    </div>
+                </div>
+                {tasks.filter((task) => task.status === status).map((task))}
+        </div>
+    );
+};
 
 export default BoardView;
