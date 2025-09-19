44580ced47067dd546196c6998285a69c9ea5e48
Chinmay-24 <naiks.chinmay@gmail.com>
2025-09-19 21:08:16 +0530

    feat: add Timeline component for task visualization

--- src/app/projects/TimelineView/index.tsx
+++ src/app/projects/TimelineView/index.tsx
@@ -1,7 +1,8 @@
 import { useAppSelector } from '@/app/redux';
 import { useGetTasksQuery } from '@/state/api';
 import { DisplayOption } from "gantt-task-react";
-import React, { useMemo, useState } from 'react'
+import { type } from 'os';
+import React, { useMemo, useState } from "react";
 
 type Props = {
     id: string;
@@ -29,15 +30,31 @@ const Timeline = ({ id, setIsModalNewTaskOpen }: Props) => {
             tasks?.map((task) => ({
                 start: new Date(task.startDate as string)
                 end: new Date(task.dueDate)
-
-            }))
+                name: task.title
+                id: `Task-${task.id}`
+                type: "task" as "TaskType",
+                progress: task.points ? (task.points /10) * 100 : 0,
+                isDisabled: false
+            })) || []
 
         )
-    }
-    )
+    }, [tasks]);
+
+    const handleViewModeChange = (
+        event: React.ChangeEvent<HTMLSelectElement>
+    ) => {
+        setDisplayOptions((prev) => ({
+            ....prev,
+            viewmode: event.target.value as ViewMode,
+        }));
+    };
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>An error occured while fetching tasks</div>;
+
 
   return 
-    <div>Timeline</div>
+    <div className="px-4 ">Timeline</div>
   
 };
 
