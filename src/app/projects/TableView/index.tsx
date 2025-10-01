e7112a9bdd9563cd4ffd6f5f7a9e79cab26edf70
Chinmay-24 <naiks.chinmay@gmail.com>
2025-10-01 23:12:23 +0530

    fix: add dataGridClassNames for styling and update status cell rendering in TableView component

--- src/app/projects/TableView/index.tsx
+++ src/app/projects/TableView/index.tsx
@@ -1,5 +1,6 @@
 import { useAppSelector } from "@/app/redux";
 import Header from "@/components/Header";
+import { dataGridClassNames } from "@/lib/utils";
 import { useGetTasksQuery } from "@/state/api";
 import {DataGrid, GridColDef } from "@mui/x-data-grid"
 import React from "react";
@@ -26,10 +27,9 @@ const columns: GridColDef[] = [
     width: 130,
     renderCell: (params) => (
       <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
-        
+        {params.value}
       </span>
-    )
-
+    ),
   },
   {
     field: "priority",
@@ -84,6 +84,8 @@ const TableView = ({id, setIsModalNewTaskOpen}: Props) => {
     <DataGrid 
       rows={tasks || []}
       columns={columns}
+      className={dataGridClassNames}
+      
       />
     <div>
       </div>
