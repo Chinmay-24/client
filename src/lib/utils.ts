f22340a2e4cac39a5a3a381a4e270a4d96708594
Chinmay-24 <naiks.chinmay@gmail.com>
2025-10-02 22:00:38 +0530

    fix: add dataGridSxStyles function for dynamic styling based on dark mode

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,2 +1,10 @@
 export const dataGridClassNames =
 "border border-gray-200 bg-white shadow dark:border-stroke-dark dark:bg-dark-secondary dark:text-gray-200"
+
+export const dataGridSxStyles = (isDarkMode: boolean) => {
+    return {
+        "& .MuiDataGrid-columnHeaders": {
+            color: `${isDarkMode ? "#e5e7eb" : ""}`
+        }
+    }
+}
