412893fecf55d952b3bc520626d9b34c22a57a9b
Chinmay-24 <naiks.chinmay@gmail.com>
2026-01-16 16:21:40 +0530

    feat: create HomePage component with task and project data retrieval

---
+++ client/src/app/home/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { Priority, useGetProjectsQuery, useGetTasksQuery } from "@/state/api";
+import React from "react";
+import { useAppSelector } from "../redux";
+
+const HomePage = () => {
+    const { data: tasks,
+            isLoading: tasksLoading, isError: tasksError} = useGetTasksQuery({projectId: parseInt("1")});
+    const { data: projects, isLoading: isProjectsLoading} = useGetProjectsQuery();
+
+    const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+    if (tasksLoading || isProjectsLoading) return <div> Loading...</div>;
+    if (tasksError || !tasks || !projects ) return <div>Error Fecthing Data </div>;
+
+    const  priorityCount = tasks.reduce(
+        (acc: Record<string, number>,task: Task) => {
+            const {priority} = task;
+            acc [priority as Priority] 
+        }
+    )
+  return <div>HomePage</div>;
+  
+};
+
+export default HomePage;
