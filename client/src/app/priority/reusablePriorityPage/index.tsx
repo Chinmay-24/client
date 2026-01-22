bf2a45bf0e472855e678ae4afb2fa76f9a82ffe8
Chinmay-24 <naiks.chinmay@gmail.com>
2026-01-22 16:23:53 +0530

    feat: add MUI components and DataGrid for task management
    - Added @emotion/react, @emotion/styled, @mui/material, and @mui/system dependencies.
    - Implemented a DataGrid in ReusablePriorityPage for displaying tasks in a table format.
    - Enhanced task display with a toggle between list and table views.
    - Updated Modal component to ensure proper z-index for modal visibility.

--- client/src/app/priority/reusablePriorityPage/index.tsx
+++ client/src/app/priority/reusablePriorityPage/index.tsx
@@ -2,12 +2,70 @@
 import { useAppSelector } from '@/app/redux';
 import Header from '@/components/Header';
 import ModalNewTask from '@/components/ModalNewTask';
+import TaskCard from '@/components/TaskCard';
+import { dataGridClassNames, dataGridSxStyles } from '@/lib/utils';
 import { Priority, Task, useGetTasksByUserQuery } from '@/state/api'
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import React, { useState } from 'react'
 
 type Props = {
     priority: Priority
 };
+
+const columns: GridColDef[] = [
+  {
+    field: "title",
+    headerName: "Tile",
+    width: 100
+  },
+  {
+    field: "description",
+    headerName: "Description",
+    width: 200,
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 130,
+    renderCell: (params) => (
+      <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+        {params.value}
+      </span>
+    ),
+  },
+  {
+    field: "priority",
+    headerName: "Priority",
+    width: 75,
+  },
+  {
+    field: "tags",
+    headerName: "Tags",
+    width:130,
+  },
+  {
+    field: "startDate",
+    headerName: "Start Date",
+    width:130,
+  },
+  {
+    field: "dueDate",
+    headerName: "Start Date",
+    width:130,
+  },
+  {
+    field: "author",
+    headerName: "Author",
+    width:150,
+    renderCell: (params) => params.value.username || "Unknown",
+  },
+  {
+    field: "assignee",
+    headerName: "Assignee",
+    width:150,
+    renderCell: (params) => params.value.username || "Unassigned",
+  },
+];
 
 const ReusablePriorityPage = ({priority}: Props) => {
     const [view, setView ] = useState("list");
@@ -33,9 +91,46 @@ const ReusablePriorityPage = ({priority}: Props) => {
                     onClick={() => setIsModalNewTaskOpen(true)}
                     >
                         Add Task
-                    </button>
+                </button>
             } 
         />
+        <div className="mb-4 flex justify-start">
+            <button
+                className={`px-4 py-2 ${
+                    view==="list" ? "bg-gray-300": "bg-white"
+                } rounded-l`}
+                onClick={() => setView("list")}>
+                    List
+            </button>
+
+            <button
+                className={`px-4 py-2 ${
+                    view==="list" ? "bg-gray-300": "bg-white"
+                } rounded-l`}
+                onClick={() => setView("table")}>
+                    Table
+            </button>
+        </div>
+        {isLoading ? (<div>Loading tasks...</div>) : view === "list" ? (
+            <div className="grid grid-cols-1 gap-4">
+                {filteredTasks?.map((task:Task) => (
+                    <TaskCard key={task.id} task={task} />
+                ))}
+            </div>
+        ) : (
+            view === "table" && filteredTasks && (
+                <div className="w-full">
+                    <DataGrid
+                        rows={filteredTasks}
+                        columns={columns}
+                        checkboxSelection
+                        getRowId={(row) => row.id}
+                        className={dataGridClassNames}
+                        sx={dataGridSxStyles(isDarkMode)}
+                />
+                </div>   
+            )
+        )}
     </div>
     );
 };
