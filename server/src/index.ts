690ec143e5cc5b20836ce12b5d4ffbd98cb2852e
Chinmay-24 <naiks.chinmay@gmail.com>
2026-07-31 18:28:42 +0530

    fix: update main entry point and script paths in package.json; change import statements to type imports in controllers; adjust TypeScript configuration for module resolution

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,11 +6,11 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 /*ROUTE IMPORTS */
-import projectRoutes from "./routes/projectRoutes.js"
-import taskRoutes from "./routes/taskRoutes.js"
-import searchRoutes from "./routes/searchRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
-import teamRoutes from "./routes/teamRoutes.js"
+import projectRoutes from "./routes/projectRoutes"
+import taskRoutes from "./routes/taskRoutes"
+import searchRoutes from "./routes/searchRoutes"
+import userRoutes from "./routes/userRoutes"
+import teamRoutes from "./routes/teamRoutes"
 
 /*CONFIGURATIONS*/
 dotenv.config();
